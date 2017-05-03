0ddcc5aab0aa19c690f49df7a404b41b4a8bea75
cga815 <carolinealexander2018@u.northwestern.edu>
2017-05-02 22:28:27 -0500

    added initial messaging interface

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  updates: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public alertCtrl: AlertController, public db: AngularFireDatabase) {
+    this.updates = db.list('/updates');
   }
+  addUpdate(){
+    let prompt = this.alertCtrl.create({
+    title: 'Update Name',
+    message: "Message you would like to send here",
+    inputs: [
+      {
+        name: 'content',
+        placeholder: 'Enter your message here',
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.updates.push({
+            content: data.content
+          });
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
 
 }
+
+

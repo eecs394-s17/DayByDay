5c8c1d88c6fbbb7ce0e742a41f88d5cddf357ad1
Safia Abdalla <safia@safia.rocks>
2017-05-07 17:46:10 -0500

    Store time of update

--- src/pages/modalContent/modalContent.ts
+++ src/pages/modalContent/modalContent.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import { Platform, ViewController } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     templateUrl: 'modalContent.html'
 })
 export class ModalContent {
-
     updates: FirebaseListObservable<any>;
     messageForm;
     messages;
 
-    constructor(public platform: Platform, public viewCtrl: ViewController, public db: AngularFireDatabase) {
+    constructor(public platform: Platform,
+        public viewCtrl: ViewController,
+        public db: AngularFireDatabase) {
       this.updates = db.list('/updates');
 
       this.messageForm = new FormGroup({
@@ -29,6 +30,7 @@ export class ModalContent {
         {
           message: 'Craig is the attending taking care of your child for this shift'
         }
+
       ]
     }
 
@@ -36,7 +38,8 @@ export class ModalContent {
       event.preventDefault();
       if(this.messageForm.value.message != 'none') {
         this.updates.push({
-          content: this.messageForm.value.message
+            content: this.messageForm.value.message,
+            timestamp: new Date().getTime()
         });
       }
       this.dismiss();

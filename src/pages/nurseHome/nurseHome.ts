0c92a2da6c2683ddb5a68e78b8f89f774306282b
Wajihuddin <wajihuddinmohammed2018@u.northwestern.edu>
2017-05-14 18:19:44 -0500

    Fixed ordering issue with updates, added a general ordering function for frontend processing (plan Bs)

--- src/pages/nurseHome/nurseHome.ts
+++ src/pages/nurseHome/nurseHome.ts
@@ -10,7 +10,24 @@ export class NurseHome {
   updates: FirebaseListObservable<any>;
 
   constructor(public modalCtrl: ModalController, public db: AngularFireDatabase) {
-    this.updates = db.list('/updates');
+
+    this.updates = db.list('/updates', {
+      query: {
+        orderByChild: 'timestamp'
+      }
+    });
+
+
+    //reusable sort function creater
+    var sort_by = function(field, reverse, primer){
+       var key = function (x) {return primer ? primer(x[field]) : x[field]};
+
+       return function (a,b) {
+    	  var A = key(a), B = key(b);
+    	  return ( (A < B) ? -1 : ((A > B) ? 1 : 0) ) * [-1,1][+!!reverse];
+       }
+    }
+
   }
   addUpdate(){
     let modal = this.modalCtrl.create(ModalContent);

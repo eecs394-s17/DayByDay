938a4278e02357bbfc345ca2be8f9ac835851015
Haridu <haridu.senadeera@gmail.com>
2017-05-14 22:02:56 -0500

    user authentication functionality

--- src/pages/signIn/signIn.ts
+++ src/pages/signIn/signIn.ts
@@ -1,9 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
+import { NavController, AlertController, Loading, LoadingController } from 'ionic-angular';
 import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ParentHome } from '../parentHome/parentHome';
 import { NurseHome } from '../nurseHome/nurseHome';
 import { Platform, MenuController, Nav } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+import { EmailValidator } from '../../validators/email';
 
 @Component({
   templateUrl: 'signIn.html'
@@ -13,14 +16,61 @@ export class SignIn {
   nurseHome = NurseHome;
   parentHome = ParentHome;
 
-  updates: FirebaseListObservable<any>;
+  public signinForm:FormGroup;  
+  public loading:Loading;
 
-  constructor(public alertCtrl: AlertController, public db: AngularFireDatabase, public navCtrl: NavController) {
-    //Get database list of messages
-    this.updates = db.list('/updates');
+  constructor(public db: AngularFireDatabase, public navCtrl: NavController,
+      private auth: AuthService, public formBuilder: FormBuilder,
+      public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
+
+    this.signinForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+    });
   }
 
-openNewRoot(page) {
+  signIn() {
+
+    // this.auth.signIn();
+
+    if (!this.signinForm.valid){
+      console.log(this.signinForm.value);
+    } else {
+      this.auth.signIn(this.signinForm.value.email, this.signinForm.value.password)
+      .then( authData => {
+        this.loading.dismiss().then( () => {
+          this.navCtrl.setRoot(ParentHome);
+        });
+        console.log("success", authData);
+      }, error => {
+        this.loading.dismiss().then( () => {
+          let alert = this.alertCtrl.create({
+            message: error.message,
+            buttons: [
+              {
+                text: "Ok",
+                role: 'cancel'
+              }
+            ]
+          });
+          alert.present();          
+        });
+      });
+
+      this.loading = this.loadingCtrl.create();
+      this.loading.present();
+    }
+  }
+
+  goToSignup(){
+    this.navCtrl.push('SignupPage');
+  }
+
+  goToResetPassword(){
+    this.navCtrl.push('PasswordResetPage');
+  }
+
+  openNewRoot(page) {
     this.navCtrl.setRoot(page);
   }
 

60478d5a9d48150bc225a8d5796f4e74f93debec
Wajihuddin <wajihuddinmohammed2018@u.northwestern.edu>
2017-05-17 17:16:50 -0500

    merge Graces previous commit

--- src/pages/nurse-send-update/nurse-send-update.ts
+++ src/pages/nurse-send-update/nurse-send-update.ts
@@ -20,6 +20,14 @@ updates: FirebaseListObservable<any>;
 messageForm;
 messages;
 children;
+attending;
+fellow;
+nurse;
+specialists;
+roundtimes;
+overnight;
+mood;
+other;
 
 constructor(public platform: Platform,
                    public viewCtrl: ViewController,
@@ -28,12 +36,23 @@ constructor(public platform: Platform,
 this.updates = db.list('/updates');
 this.messages = db.list('/messages');
 this.children = db.list('/children');
+this.attending = db.list('/staff/attending');
+this.fellow = db.list('/staff/fellow');
+this.nurse = db.list('/staff/nurse');
+this.specialists = db.list('/specialists');
+this.roundtimes = db.list('/roundtimes');
+this.overnight = db.list('/overnight');
+this.mood = db.list('/mood');
+this.other = db.list('/other');
+
 
 this.messageForm = new FormGroup({
                                   "child": new FormControl({value: 'none', disabled: false}),
                                   "message": new FormControl({value: 'none', disabled: false}),
                                   });
 }
+
+//update this function to reflect new variable names
 
   doSubmit(event) {
     event.preventDefault();

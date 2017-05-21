5e2bcfb980ad47b21fd44211b50190bc47c73ead
Safia Abdalla <safia@safia.rocks>
2017-05-21 18:08:45 -0500

    Add initial code for conditional side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,24 +20,45 @@ import { ChildEditor } from '../pages/child-editor/child-editor'
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
   rootPage:any = SignIn;
+  currentPage:any = SignIn;
   pages: Array<{title: string, component: any}>;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public menu: MenuController,
-) {
-    platform.ready().then(() => {
-    this.pages = [
-      { title: 'Home', component: ParentHome },
-      { title: 'Information', component: HospitalInfo },
-      { title: 'Staff', component: Staff },
-      { title: 'Nurse Updates', component: NurseHome },
-      { title: 'Child Management', component: ChildManagement },
-    ];
+  constructor(platform: Platform,
+      statusBar: StatusBar,
+      splashScreen: SplashScreen,
+      public menu: MenuController) {
+      platform.ready().then(() => {
+
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      });
+
+    }
+
+  ngAfterViewInit() {
+  this.nav.viewDidEnter.subscribe((data) => {
+  console.log(data);
+      this.currentPage = data.component.name;
+      console.log(this.currentPage);
+      if (this.currentPage === 'ParentHome') {
+        this.pages = [
+          { title: 'Feed', component: ParentHome },
+          { title: 'Staff', component: Staff },
+          { title: 'Nurse Updates', component: NurseHome },
+        ];
+      } else {
+        this.pages = [
+          { title: 'Nurse Homepage', component: NurseHome },
+          { title: 'Information', component: HospitalInfo },
+          { title: 'Staff', component: Staff },
+          { title: 'Child Management', component: ChildManagement },
+        ];
+      }
     });
-  }
+}
+
   openPage(page) {
     // close the menu when clicking a link from the menu
     this.menu.close();

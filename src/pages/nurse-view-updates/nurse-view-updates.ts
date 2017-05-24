dc1920fd22ce9884b7810e9b687c69912daa49b8
Wajihuddin <wajihuddinmohammed2018@u.northwestern.edu>
2017-05-23 21:34:33 -0500

    Made the update view UI the same for nurse side

--- src/pages/nurse-view-updates/nurse-view-updates.ts
+++ src/pages/nurse-view-updates/nurse-view-updates.ts
@@ -10,30 +10,64 @@ import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database
 */
 @IonicPage()
 @Component({
-            selector: 'page-nurse-view-updates',
-            templateUrl: 'nurse-view-updates.html',
-            })
+  selector: 'page-nurse-view-updates',
+  templateUrl: 'nurse-view-updates.html',
+})
 export class NurseViewUpdates {
-updates: FirebaseListObservable<any>;
+  updates: FirebaseListObservable<any>;
 
-constructor(public db: AngularFireDatabase) {
+  constructor(public db: AngularFireDatabase) {
 
-this.updates = db.list('/updates', {
-                       query: {
-                       orderByChild: 'timestamp'
-                       }
-                       });
+    this.updates = db.list('/updates', {
+      query: {
+        orderByChild: 'timestamp'
+      }
+    });
 
 
-//reusable sort function creater
-var sort_by = function(field, reverse, primer){
-var key = function (x) {return primer ? primer(x[field]) : x[field]};
+    //reusable sort function creater
+    var sort_by = function(field, reverse, primer) {
+      var key = function(x) { return primer ? primer(x[field]) : x[field] };
 
-return function (a,b) {
-var A = key(a), B = key(b);
-return ( (A < B) ? -1 : ((A > B) ? 1 : 0) ) * [-1,1][+!!reverse];
-}
-}
+      return function(a, b) {
+        var A = key(a), B = key(b);
+        return ((A < B) ? -1 : ((A > B) ? 1 : 0)) * [-1, 1][+!!reverse];
+      }
+    }
 
-}
+  }
+  timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var meridian;
+    if (hour > 12) {
+      hour = hour - 12;
+      meridian = 'pm';
+    }
+    else if (hour == 12) {
+      meridian = 'pm';
+    }
+    else if (hour == 0) {
+      hour = 12;
+      meridian = 'am';
+    }
+    else {
+      meridian = 'am';
+    }
+
+    var min = a.getMinutes();
+    var minStr;
+    if (min < 10) {
+      minStr = '0' + min;
+    }
+    else {
+      minStr = min;
+    }
+    var time = month + ' ' + date + ' at ' + hour + ':' + minStr + ' ' + meridian;
+    return time;
+  }
 }

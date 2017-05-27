c93a6db2e7087ad8566643ae1706f17bc77ac29c
Safia Abdalla <safia@safia.rocks>
2017-05-26 20:15:25 -0500

    Add logout button to nurse page

--- src/pages/nurseHome/nurseHome.ts
+++ src/pages/nurseHome/nurseHome.ts
@@ -1,6 +1,10 @@
 import {Component} from '@angular/core';
+import { NavController } from 'ionic-angular';
 import { NurseViewUpdates } from '../nurse-view-updates/nurse-view-updates';
 import { NurseSendUpdate } from '../nurse-send-update/nurse-send-update';
+import { SignIn } from '../signIn/signIn';
+
+import { AuthService } from '../../providers/auth-service';
 
 @Component({
   templateUrl: 'NurseHome.html'
@@ -8,8 +12,16 @@ import { NurseSendUpdate } from '../nurse-send-update/nurse-send-update';
 export class NurseHome {
     sendUpdate: any;
     viewUpdates: any;
-  constructor() {
+  constructor(private auth: AuthService, public navCtrl: NavController) {
     this.sendUpdate = NurseSendUpdate;
     this.viewUpdates = NurseViewUpdates ;
+  }
+  logout() {
+    var that = this;
+    this.auth.signOut().then(function() {
+      that.navCtrl.push(SignIn);
+    }).catch(function(error) {
+        console.log(error);
+    });
   }
 }
